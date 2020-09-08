5e17e66cbdaf798e524feacec122ab9ddb4a0395
iamshaunjp <shrinivasshah05@gmail.com>
2020-09-08 12:23:07 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const keys = require("./config/keys");
+const mongoose = require("mongoose");
+require("./models/User");
+require("./services/passport");
+mongoose.connect(keys.mongoURI);
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send({ data: "data" });
-});
-const PORT = process.env.PORT || 5000
+require("./routes/authRoutes")(app);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
